81db9bc1f1abb6403ad15d048c1591234d422db2
MarcosLp <51751618+MarcosLp69@users.noreply.github.com>
2020-07-14 00:11:56 +0200

    Add files via upload

---
+++ bin/ghost.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+args = sys.argv
+args.pop(0)
+
+if len(args) == 0:
+    args.append(None)
+
+if args[0] in ["--version","-v"]:
+    print("Ghost Package Manager for pacman and yay")
+    print("Version: Dev 3")
+
+elif args[0] in ["install","-S","-i"]:
+    args.pop(0)
+    os.system(f"sudo pacman -S {' '.join(args)}")
+    
+elif args[0] in ["upgrade","update","-u"]:
+    args.pop(0)
+    os.system(f"sudo pacman -Syu {' '.join(args)}")
+    
+elif args[0] in ["aur","-a","-aur"]:
+    args.pop(0)
+    os.system(f"yay -S {' '.join(args)}")
+
+elif args[0] in ["remove","-R","--remove"]:
+    args.pop(0)
+    os.system(f"sudo pacman -R {' '.join(args)}")
+
+else:
+    print("Invalid Command")
---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo cp ./bin/ghost /usr/bin/ghost
+sudo cp ./bin/ghost.py ~/.ghost/ghost.py
+
+sudo chmod +x /usr/bin/ghost
+echo "Ghost has been installed!"
+echo "To check if the install was correct run ghost --version"
